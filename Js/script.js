72d5e54b256de9cfd1da0abbb1db9b2a84d800d8
Pratik Dole <pdoleofficial@gmail.com>
2023-02-05 14:37:27 +0530

    first checkout

---
+++ Js/script.js
@@ -0,0 +1,12 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '577d0e8f5cmsh9bec304960cd15ap146d89jsn9b5c65d7a33c',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
